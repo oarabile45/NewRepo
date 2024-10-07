369d81dc342f3dc4d8e3737cfc18b892c46b2522
Ayakha Cengani <s224008536@mandela.ac.za>
2024-10-07 20:34:34 +0200

    Added purchase order report

--- Cool Co Fridge Management/Controllers/PurchaseOrderController.cs
+++ Cool Co Fridge Management/Controllers/PurchaseOrderController.cs
@@ -3,6 +3,9 @@ using Cool_Co_Fridge_Management.Models;
 using Cool_Co_Fridge_Management.Data;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
+using PdfSharp.Pdf;
+
 
 
 namespace Cool_Co_Fridge_Management.Controllers
@@ -197,6 +200,75 @@ namespace Cool_Co_Fridge_Management.Controllers
                 return NotFound();
             }
             return View(order);
+        }
+
+        public IActionResult GeneratePurchaseOrderReport()
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            //get data from database
+            var reportData = (from po in applicationDbContext.orders
+                              join dn in applicationDbContext.DeliveryNotes on po.DeliveryNoteId equals dn.DeliveryNoteId into pdn
+                              from dn in pdn.DefaultIfEmpty()
+                              join s in applicationDbContext.suppliers on po.SupplierId equals s.SupplierId
+                              join ft in applicationDbContext.fridge_type on po.FridgeTypeID equals ft.FridgeTypeID
+                              join os in applicationDbContext.orderStatus on po.OrderStatusId equals os.OrderStatusId
+                              select new OrderReportViewModel 
+                              { 
+                                  OrderID = po.OrderID,
+                                  ItemName = po.ItemName,
+                                  Quantity = po.Quantity,
+                                  OrderedDate = po.OrderedDate,
+                                  Supplier = s.SupplierName,
+                                  FridgeType = ft.FridgeType,
+                                  OrderStatus = os.OrderDesc,
+                                  DeliveryDate = dn != null ? dn.DeliveredDate : null,
+                                  DeliveryNoteDetails = dn != null ? dn.DeliveryDetails: "N/A"
+
+                              }).ToList();
+
+
+            //generating Excel file
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("OrderReportViewModel");
+
+                //Headers
+                worksheet.Cells[1, 1].Value = "OrderID";
+                worksheet.Cells[1, 2].Value = "Item Name";
+                worksheet.Cells[1, 3].Value = "Quantity";
+                worksheet.Cells[1, 4].Value = "Ordered Date";
+                worksheet.Cells[1, 5].Value = "Supplier Name";
+                worksheet.Cells[1, 6].Value = "Fridge Type";
+                worksheet.Cells[1, 7].Value = "Order Status";
+                worksheet.Cells[1, 8].Value = "Delivery Date";
+                worksheet.Cells[1, 9].Value = "Delivery Note Details";
+
+                //Add the data
+                for(int i = 0; i< reportData.Count; i++)
+                {
+                    worksheet.Cells[i + 2, 1].Value = reportData[i].OrderID;
+                    worksheet.Cells[i + 2, 2].Value = reportData[i].ItemName;
+                    worksheet.Cells[i + 2, 3].Value = reportData[i].Quantity;
+                    worksheet.Cells[i + 2, 4].Value = reportData[i].OrderedDate.ToString();
+                    worksheet.Cells[i + 2, 5].Value = reportData[i].Supplier;
+                    worksheet.Cells[i + 2, 6].Value = reportData[i].FridgeType;
+                    worksheet.Cells[i + 2, 7].Value = reportData[i].OrderStatus;
+                    worksheet.Cells[i + 2, 8].Value = reportData[i].DeliveryDate?.ToString();
+                    worksheet.Cells[i + 2, 9].Value = reportData[i].DeliveryNoteDetails;
+
+                }
+                worksheet.Cells[worksheet.Dimension.Address].AutoFitColumns();
+
+                //export to excel file
+                var excelFile = new MemoryStream();
+                package.SaveAs(excelFile);
+                excelFile.Position = 0;
+
+                string excelName = $"Purchase Order Report - {DateTime.Now:yyyyMMddHH}.xlsx";
+                return File(excelFile, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", excelName);
+
+            }
         }
     }
 }
---
+++ Cool Co Fridge Management/Models/OrderReportViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Cool_Co_Fridge_Management.Models
+{
+    public class OrderReportViewModel
+    {
+        public int OrderID { get; set; }
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+        public DateOnly OrderedDate { get; set; }
+        public string Supplier { get; set; }
+        public string FridgeType { get; set; }
+        public string OrderStatus { get; set; }
+        public DateTime? DeliveryDate { get; set; }
+        public string DeliveryNoteDetails { get; set; }
+    }
+}
