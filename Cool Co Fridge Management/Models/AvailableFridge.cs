740f849b7a88f26c852ac1acda5c8ac40525792d
Dyl19 <114477847+Dyl19@users.noreply.github.com>
2024-10-16 22:27:39 +0200

    updated work

---
+++ Cool Co Fridge Management/Models/AvailableFridge.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cool_Co_Fridge_Management.Models
+{
+    public class AvailableFridge
+    {
+        public int StockID { get; set; } 
+        public string ItemName { get; set; }
+        public int FridgeTypeID { get; set; }
+        public string FridgeType { get; set; }
+        public bool Availability { get; set; }
+    }
+
+}
